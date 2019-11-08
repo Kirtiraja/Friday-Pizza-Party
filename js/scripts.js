86ef1fefcdbffa458b62a0eaccb54cb8090a307b
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-11-08 10:57:14 -0800

    add business-side logic to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+//Business logic---------------------------------------------
+function PizzaMaker(size,toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = 0;
+}
+
+PizzaMaker.getPrice = function() {
+  this.toppings.forEach(function(topping){
+    this.price ++ 1 ;
+  }
+      if (this.size === "small") {
+        this.price ++ 13;
+      }else if (this.size === "medium") {
+        this.price ++ 15;
+      }else if (this.size === "large") {
+        this.price ++ 17;
+      }
+      return this.price;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//User interface logic------------------------------------------
